f173af523b0c972f66e5bfffdfe4cadedd31f8e7
Thiago C. dos Santos <104088057+Thiago-CSantos@users.noreply.github.com>
2023-10-17 22:35:02 -0300

    Add files via upload

---
+++ aula17_10/src/main/java/com/mycompany/aula17_10/Aula17_10.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.aula17_10;
+
+public class Aula17_10 {
+
+    public static void main(String[] args) {
+        AulaCheckBox frame = new AulaCheckBox();
+        
+        AulaRadioButton frame2 = new AulaRadioButton();
+        
+        AulaComboBox frame3 = new AulaComboBox();
+        frame3.setVisible(true);
+        
+        
+    }
+}

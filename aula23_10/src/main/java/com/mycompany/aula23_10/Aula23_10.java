fc3a91e8e5f96577d5decffb20b508669c3ae1ae
Thiago C. dos Santos <104088057+Thiago-CSantos@users.noreply.github.com>
2023-10-23 20:43:08 -0300

    Add files via upload

---
+++ aula23_10/src/main/java/com/mycompany/aula23_10/Aula23_10.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.aula23_10;
+
+public class Aula23_10 {
+
+    public static void main(String[] args) {
+        Tabela jframe = new Tabela();
+        
+        jframe.setVisible(true);
+    }
+}

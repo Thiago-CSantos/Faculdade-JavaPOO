08f2302c485930e2989ca54cc4de04b088a359af
Thiago C. dos Santos <104088057+Thiago-CSantos@users.noreply.github.com>
2024-06-04 22:43:43 -0300

    Add files via upload

--- projetoJDBC/src/main/java/model/DAOConta.java
+++ projetoJDBC/src/main/java/model/DAOConta.java
@@ -15,26 +15,27 @@ public class DAOConta {
     private static final String USER = "root";
     private static final String PASS = "1234";
     private Connection con = null;
-    public List<Conta> selectConta() throws SQLException{
+
+    public List<Conta> selectConta() throws SQLException {
         con = DriverManager.getConnection(DB_URL, USER, PASS);
-        
+
         String sql = "select * from Conta;";
-        
+
         PreparedStatement comando = con.prepareStatement(sql);
         ResultSet result = comando.executeQuery();
-        
+
         List<Conta> contas = new ArrayList<>();
-        
-        while(result.next()){
+
+        while (result.next()) {
 //            Conta conta = new Conta(result.getString(2), result.getString(3), result.getDouble(4));
             contas.add(new Conta(result.getString(2), result.getString(3), result.getDouble(4)));
         }
-        
+
         return contas;
     }
 
     public void criaTabelaConta() {
-        
+
         Statement stmt = null;
         try {
             // Load the JDBC driver
@@ -84,23 +85,51 @@ public class DAOConta {
             }
         }
     }
-    
-    public void insertConta(Conta conta) throws SQLException, ClassNotFoundException{
+
+    public void insertConta(Conta conta) {
         Connection con = null;
         // Load the JDBC driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            // Establish the connection
-            con = DriverManager.getConnection(DB_URL, USER, PASS);
-            
-            String sql = 
-            "insert into Conta(numConta, numAg,saldo) Values(?,?,?)";
-            PreparedStatement comando = con.prepareStatement(sql);
-            comando.setString(1, conta.getNumAg());
-            comando.setString(2, conta.getNumAg());
-            comando.setDouble(3, conta.getSaldo());
-            
-            comando.executeUpdate();
-            con.close();
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // Establish the connection
+        con = DriverManager.getConnection(DB_URL, USER, PASS);
+
+        String sql
+                = "insert into Conta(numConta, numAg,saldo) Values(?,?,?)";
+        PreparedStatement comando = con.prepareStatement(sql);
+        comando.setString(1, conta.getNumAg());
+        comando.setString(2, conta.getNumAg());
+        comando.setDouble(3, conta.getSaldo());
+
+        comando.executeUpdate();
+        con.close();
     }
-   
+
+    public void transferencia(Conta conta1, Conta conta2, double valor) throws SQLException {
+        try {
+            con = DriverManager.getConnection(DB_URL, USER, PASS);
+            try {
+
+                con.setAutoCommit(false);
+
+                String sql = "UPDATE Conta set saldo = ? where idConta = ?";
+
+                PreparedStatement comando = con.prepareStatement(sql);
+                comando.setDouble(1, conta1.getSaldo() - 1500);
+                comando.setInt(2, conta1.getIdConta());
+                comando.executeUpdate();
+
+                comando.setDouble(1, conta2.getSaldo() + 1500);
+                comando.setInt(2, conta2.getIdConta());
+                comando.executeUpdate();
+                con.commit();
+            } catch (SQLException e) {
+                con.rollback();
+            } finally {
+                con.setAutoCommit(true);
+            }
+        } catch (SQLException e) {
+            throw e;
+        }
+
+    }
 }

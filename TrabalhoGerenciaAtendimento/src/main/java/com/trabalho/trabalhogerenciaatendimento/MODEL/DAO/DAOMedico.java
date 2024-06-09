cf195b32c4834cacd7ca398de28237e1c1adc5de
Rafael Almeida V <104868359+rafaelalmeidaV@users.noreply.github.com>
2024-06-09 13:17:16 -0300

    feat: medico

---
+++ TrabalhoGerenciaAtendimento/src/main/java/com/trabalho/trabalhogerenciaatendimento/MODEL/DAO/DAOMedico.java
@@ -0,0 +1,87 @@
+package com.trabalho.trabalhogerenciaatendimento.MODEL.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.trabalho.trabalhogerenciaatendimento.MODEL.Medico;
+import com.trabalho.trabalhogerenciaatendimento.MODEL.Paciente;
+import com.trabalho.trabalhogerenciaatendimento.MODEL.Enum.Especialidade;
+import com.trabalho.trabalhogerenciaatendimento.MODEL.Enum.Sexo;
+
+public class DAOMedico {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/GerenciaAtendimento";
+    private static final String USER = "root";
+    private static final String PASS = "123456";
+    private Connection conexao = null;
+    private List<Medico> medicoList = new ArrayList<>();
+
+    public DAOMedico() {
+    }
+
+    protected void conectar() throws SQLException {
+        conexao = DriverManager.getConnection(DB_URL, USER, PASS);
+    }
+
+    protected void desconectar() throws SQLException {
+        conexao.close();
+    }
+
+    public List<Medico> listarMedico() {
+        try {
+            conectar();
+            String sql = "SELECT CRM, nome, rg, cpf, dataNascimento, foto, especialidad FROM Medico";
+            PreparedStatement com = conexao.prepareStatement(sql);
+
+            ResultSet result = com.executeQuery();
+
+            while (result.next()) {
+                String CRM = result.getString(1);
+                String nome = result.getString(2);
+                String rg = result.getString(3);
+                String cpf = result.getString(4);
+                String dataNascimento = result.getString(5);
+                String foto = result.getString(6);
+                Especialidade especialidade = Especialidade.valueOf(result.getString(7));
+                medicoList.add(new Medico(CRM, nome, rg, cpf, dataNascimento, foto, especialidade));
+            }
+
+            desconectar();
+            System.out.println(medicoList);
+            return medicoList;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void cadastrarPaciente(Medico medico) {
+        try {
+            conectar();
+            String sql = "INSERT INTO Medico (CRM, nome, rg, cpf, dataNascimento, foto, especialidade) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement com = conexao.prepareStatement(sql);
+
+            com.setString(1, medico.getCRM());
+            com.setString(2, medico.getNome());
+            com.setString(3, medico.getRg());
+            com.setString(4, medico.getCpf());
+            com.setString(5, medico.getDataNascimento());
+            com.setString(6, medico.getFoto());
+            com.setString(7, medico.getEspecialidade().toString());
+
+            com.executeUpdate();    
+            desconectar();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erro ao cadastrar paciente: " + e.getMessage());
+        }
+    }
+}

aa075548db19624ecef8774c33a74a082a1906c3
Rafael Almeida V <104868359+rafaelalmeidaV@users.noreply.github.com>
2024-06-09 20:54:15 -0300

    feat: tela de listagem de medicos

--- TrabalhoGerenciaAtendimento/src/main/java/com/trabalho/trabalhogerenciaatendimento/Controller/MedicoController.java
+++ TrabalhoGerenciaAtendimento/src/main/java/com/trabalho/trabalhogerenciaatendimento/Controller/MedicoController.java
@@ -1,7 +1,15 @@
 package com.trabalho.trabalhogerenciaatendimento.Controller;
 
 import com.trabalho.trabalhogerenciaatendimento.MODEL.DAO.DAOMedico;
+
+import java.util.List;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 import com.trabalho.trabalhogerenciaatendimento.MODEL.Medico;
+import com.trabalho.trabalhogerenciaatendimento.MODEL.Paciente;
+import com.trabalho.trabalhogerenciaatendimento.MODEL.Responsavel;
 
 public class MedicoController {
     
@@ -13,6 +21,28 @@ public class MedicoController {
     
     public void cadastrarMedico(Medico medico){
         dao.cadastrarMedico(medico);
+    }
+
+    public void gerarTableModel(JTable tabelaMedicos) {
+
+        DefaultTableModel modelo = (DefaultTableModel) tabelaMedicos.getModel();
+        Object[] objetos = new Object[7];
+
+        List<Medico> item = dao.listarMedico();
+
+        for (Medico medico : item) {
+            objetos[0] = medico.getCRM();
+            objetos[1] = medico.getNome();
+            objetos[2] = medico.getRg();
+            objetos[3] = medico.getCpf();
+            objetos[4] = medico.getDataNascimento();   
+            objetos[5] = medico.getFoto();  
+            objetos[6] = medico.getEspecialidade();
+
+
+            modelo.addRow(objetos);
+        }
+
     }
     
 }
